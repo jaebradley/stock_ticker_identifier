91212836c683b58e3a0de22f8a1a125f424d0fff
jaebradley <jae.b.bradley@gmail.com>
2017-10-02 19:45:18 -0400

    update

---
+++ publicly_traded_companies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Exchange(models.Model):
+    name = models.TextField(null=False, unique=True)
+    nickname = models.TextField()
+
+
+class Company(models.Model):
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
+    name = models.TextField(null=False)
+    ticker = models.TextField(null=False)
+    ipo_year = models.IntegerField()
+    sector = models.TextField()
+    industry = models.TextField()
