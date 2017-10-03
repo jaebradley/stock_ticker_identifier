107ed0f58458ae6be407bea37f252bd6bd0e69ed
jaebradley <jae.b.bradley@gmail.com>
2017-10-02 22:34:28 -0400

    fixing logging (for now)

--- publicly_traded_companies/inserters.py
+++ publicly_traded_companies/inserters.py
@@ -6,6 +6,10 @@ from publicly_traded_companies.models import Exchange, Company
 from publicly_traded_companies.constants import DOWNLOAD_NASDAQ_COMPANIES_URL, DOWNLOAD_NYSE_COMPANIES_URL, \
     DOWNLOAD_AMEX_COMPANIES_URL
 
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def insert_exchanges():
     exchanges = {
@@ -15,28 +19,28 @@ def insert_exchanges():
     }
 
     for exchange_name, exchange_nickname in exchanges.items():
-        logging.info('Get or creating exchange: {exchange_name}'.format(exchange_name=exchange_name))
+        logger.info('Get or creating exchange: {exchange_name}'.format(exchange_name=exchange_name))
         exchange, created = Exchange.objects.get_or_create(name=exchange_name, nickname=exchange_nickname)
-        logging.info('Exchange: {exchange} was created: {created}'.format(exchange=exchange, created=created))
+        logger.info('Exchange: {exchange} was created: {created}'.format(exchange=exchange, created=created))
 
 
 def insert_nasdaq_companies():
-    logging.info('Inserting NASDAQ companies')
+    logger.info('Inserting NASDAQ companies')
     insert_companies_for_exchange(exchange=Exchange.objects.get(nickname='NASDAQ'), url=DOWNLOAD_NASDAQ_COMPANIES_URL)
 
 
 def insert_nyse_companies():
-    logging.info('Inserting NYSE companies')
+    logger.info('Inserting NYSE companies')
     insert_companies_for_exchange(exchange=Exchange.objects.get(nickname='NASDAQ'), url=DOWNLOAD_NYSE_COMPANIES_URL)
 
 
 def insert_amex_companies():
-    logging.info('Inserting AMEX companies')
+    logger.info('Inserting AMEX companies')
     insert_companies_for_exchange(exchange=Exchange.objects.get(nickname='AMEX'), url=DOWNLOAD_AMEX_COMPANIES_URL)
 
 
 def insert_companies_for_exchange(exchange, url):
-    logging.info('Inserting companies for exchange: {exchange} from URL: {url}'.format(exchange=exchange, url=url))
+    logger.info('Inserting companies for exchange: {exchange} from URL: {url}'.format(exchange=exchange, url=url))
     content = requests.get(url)
     companies = list(csv.reader(content.text.splitlines(), delimiter=','))
     # remove first row which are column headers
@@ -54,4 +58,4 @@ def insert_companies_for_exchange(exchange, url):
         industry = row[7]
         company, created = Company.objects.get_or_create(exchange=exchange, name=name, ticker=ticker, ipo_year=ipo_year,
                                                          sector=sector, industry=industry)
-        logging.info('Company: {company} was created: {created}'.format(company=company, created=created))
+        logger.info('Company: {company} was created: {created}'.format(company=company, created=created))
