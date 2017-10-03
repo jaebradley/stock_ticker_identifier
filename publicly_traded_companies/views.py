c3e2637d543951f74c21a2380834df06bd3f4cf4
jaebradley <jae.b.bradley@gmail.com>
2017-10-03 00:54:29 -0400

    add searching

--- publicly_traded_companies/views.py
+++ publicly_traded_companies/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import SearchFilter
 
 from publicly_traded_companies.models import Exchange, Company
 from publicly_traded_companies.serializers import ExchangeSerializer, CompanySerializer
@@ -14,6 +15,8 @@ class ExchangesView(ListAPIView):
     queryset = Exchange.objects.all().order_by('name')
     serializer_class = ExchangeSerializer
     pagination_class = StandardLimitOffsetPagination
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'nickname')
 
 
 class ExchangeView(RetrieveAPIView):
@@ -25,6 +28,8 @@ class CompaniesView(ListAPIView):
     queryset = Company.objects.all().order_by('name')
     serializer_class = CompanySerializer
     pagination_class = StandardLimitOffsetPagination
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'ticker')
 
 
 class CompanyView(RetrieveAPIView):
