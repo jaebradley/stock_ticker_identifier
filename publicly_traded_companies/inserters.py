68d422577ab3ba883c566ca714b62356d03763f3
jaebradley <jae.b.bradley@gmail.com>
2017-10-20 23:04:14 -0400

    add company details inserter

--- publicly_traded_companies/inserters.py
+++ publicly_traded_companies/inserters.py
@@ -2,7 +2,7 @@ import csv
 import requests
 import logging
 
-from publicly_traded_companies.models import Exchange, Company
+from publicly_traded_companies.models import Exchange, Company, Industry, Sector
 from publicly_traded_companies.constants import DOWNLOAD_NASDAQ_COMPANIES_URL, DOWNLOAD_NYSE_COMPANIES_URL, \
     DOWNLOAD_AMEX_COMPANIES_URL
 
@@ -55,3 +55,42 @@ def insert_companies_for_exchange(exchange, url):
         ticker = row[0]
         company, created = Company.objects.update_or_create(exchange=exchange, ticker=ticker, defaults={'name': name})
         logger.info('Company: {company} was created: {created}'.format(company=company, created=created))
+
+
+def insert_all_company_details():
+    for company in Company.objects.all():
+        insert_company_details(company)
+
+
+def insert_company_details(company):
+    r = requests.get(url='https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}'.format(ticker=company.ticker),
+                     params={
+                         'formatted': True,
+                         'modules': 'assetProfile'
+                     })
+
+    r.raise_for_status()
+
+    raw_company_details = r.json()
+
+    company_details = raw_company_details['quoteSummary']['result'][0]['assetProfile']
+
+    industry, created = Industry.objects.get_or_create(name=company_details['industry'])
+    sector, created = Sector.objects.get_or_create(name=company_details['sector'])
+
+    company.industry = industry
+    company.sector = sector
+
+    company.address_line_1 = company_details.get('address1')
+    company.address_line_2 = company_details.get('address2')
+    company.city = company_details.get('city')
+    company.state = company_details.get('state')
+    company.country = company_details.get('country')
+    company.zip_code = company_details.get('zip')
+    company.fax_number = company_details.get('fax')
+    company.phone_number = company_details.get('phone')
+    company.website_url = company_details.get('website')
+    company.employee_count = company_details.get('fullTimeEmployees')
+    company.business_summary = company_details.get('longBusinessSummary')
+
+    company.save()
