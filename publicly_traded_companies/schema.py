ff7db5c892cd745fe89d4624fbc252aaad2692a4
jaebradley <jae.b.bradley@gmail.com>
2017-10-24 19:29:15 -0400

    update graphql schema

--- publicly_traded_companies/schema.py
+++ publicly_traded_companies/schema.py
@@ -2,7 +2,8 @@ from graphene import relay, AbstractType, Schema, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from publicly_traded_companies.models import Exchange as ExchangeModel, Company as CompanyModel
+from publicly_traded_companies.models import Exchange as ExchangeModel, Company as CompanyModel, \
+    Industry as IndustryModel, Sector as SectorModel
 
 
 class ExchangeNode(DjangoObjectType):
@@ -15,12 +16,34 @@ class ExchangeNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
+class IndustryNode(DjangoObjectType):
+    class Meta:
+        model = IndustryModel
+        filter_fields = {
+            'name': ['exact', 'icontains']
+        }
+        interfaces = (relay.Node, )
+
+
+class SectorNode(DjangoObjectType):
+    class Meta:
+        model = SectorModel
+        filter_fields = {
+            'name': ['exact', 'icontains']
+        }
+        interfaces = (relay.Node, )
+
+
 class CompanyNode(DjangoObjectType):
     class Meta:
         model = CompanyModel
         filter_fields = {
             'name': ['exact', 'icontains'],
             'ticker': ['exact', 'icontains'],
+            'sector__id': ['exact'],
+            'sector__name': ['exact', 'icontains'],
+            'industry__id': ['exact'],
+            'industry__name': ['exact', 'icontains'],
         }
         interfaces = (relay.Node, )
 
@@ -31,6 +54,12 @@ class Query(AbstractType, ObjectType):
 
     company = relay.Node.Field(CompanyNode)
     all_companies = DjangoFilterConnectionField(CompanyNode)
+
+    industry = relay.Node.Field(IndustryNode)
+    all_industries = DjangoFilterConnectionField(IndustryNode)
+
+    sector = relay.Node.Field(SectorNode)
+    all_sectors = DjangoFilterConnectionField(SectorNode)
 
 
 schema = Schema(query=Query)
